fdcccff559f3ed31dd6e856d17f37571a56046d9
VicWoof <victorwoof@yahoo.com.hk>
2023-03-06 13:01:44 +0000

    MVP

--- src/app.py
+++ src/app.py
@@ -1,34 +1,88 @@
-from dash import Dash, dcc, Output, Input  # pip install dash
+from dash import Dash, dcc, Output, Input, html  # pip install dash
 import dash_bootstrap_components as dbc    # pip install dash-bootstrap-components
 import plotly.express as px
+import pandas as pd
 
-# incorporate data into app
-df = px.data.medals_long()
+# create initial data
+def create_data(potsize, apr, monthly_withdrawal, years_to_forecast, start_age):
+    df = pd.DataFrame()
+
+    data = []
+    balance = potsize
+    age = start_age
+    data.append([str(start_age)+'.0', balance])
+    while age <= (start_age + years_to_forecast + 1):
+        month = 1
+        while month <= 12:
+            balance = (balance - monthly_withdrawal) * (1 + apr/100/12)
+            if balance <= 0 : balance = 0
+            item = [str(age)+'.'+str(month), balance]
+            month += 1
+            data.append(item)
+        age += 1
+
+    df = pd.DataFrame(data, columns=['Age', 'Balance'])
+    return df
+
+df = create_data(500000,5,2000,50,57)
 
 # Build your components
 app = Dash(__name__, external_stylesheets=[dbc.themes.VAPOR])
 server = app.server
-mytitle = dcc.Markdown(children='# App that analyzes Olympic medals')
 mygraph = dcc.Graph(figure={})
-dropdown = dcc.Dropdown(options=['Bar Plot', 'Scatter Plot'],
-                        value='Bar Plot',  # initial value displayed when page first loads
-                        clearable=False)
+potsize = dcc.Input(type='number', min=1, step=1, value=500000, debounce=True, required=True)
+apr = dcc.Input(type='number', value=5, debounce=True)
+monthly_withdrawal = dcc.Input(type='number', min=0, step=1, value=2000, debounce=True)
+years_to_forecast = dcc.Input(type='number', min=1, step=1, value=50, debounce=True)
+start_age = dcc.Input(type='number', min=1, step=1, value=57, debounce=True)
 
 # Customize your own Layout
-app.layout = dbc.Container([mytitle, mygraph, dropdown])
+app.layout = html.Div(children=[
+    html.H1(
+        children='Pension Drawdown',
+        style={
+            'textAlign': 'center'
+        }
+    ),
+        
+    mygraph,
+
+    html.Div(children=[
+        html.Div(children=[
+            html.Label('Pot Size'),
+            potsize
+        ]),
+        html.Div(children=[
+            html.Label('Annual Return Rate'),
+            apr
+        ]),
+        html.Div(children=[
+            html.Label('Monthly Withdrawl'),
+            monthly_withdrawal
+        ]),
+        html.Div(children=[
+            html.Label('Years to Forecast'),
+            years_to_forecast,
+        ]),
+        html.Div(children=[
+            html.Label('Starting Age'),
+            start_age
+        ])
+    ])
+])
 
 # Callback allows components to interact
 @app.callback(
     Output(mygraph, component_property='figure'),
-    Input(dropdown, component_property='value')
+    [Input(potsize, component_property='value'),
+     Input(apr, component_property='value'),
+     Input(monthly_withdrawal, component_property='value'),
+     Input(years_to_forecast, component_property='value'),
+     Input(start_age, component_property='value')]
 )
-def update_graph(user_input):  # function arguments come from the component property of the Input
-    if user_input == 'Bar Plot':
-        fig = px.bar(data_frame=df, x="nation", y="count", color="medal")
-
-    elif user_input == 'Scatter Plot':
-        fig = px.scatter(data_frame=df, x="count", y="nation", color="medal",
-                         symbol="medal")
+def update_graph(potsize, apr, monthly_withdrawal, years_to_forecast, start_age):  # function arguments come from the component property of the Input
+    df = create_data(potsize, apr, monthly_withdrawal, years_to_forecast, start_age)
+    fig = px.line(data_frame=df, x="Age", y="Balance")
 
     return fig  # returned objects are assigned to the component property of the Output
 
